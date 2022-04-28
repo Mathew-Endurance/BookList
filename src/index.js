9b548d595690a39502672153be9ae570c136fbf2
endurance <enduremathew@gmail.com>
2022-04-28 10:56:34 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function BookList() {
 const Person = () => <h2>this is new header</h2>;
 const Message = () => <p>this is the paragraph</p>;
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<BookList />, document.getElementById("root"));
 
 //function Greeting() {
 //return (
